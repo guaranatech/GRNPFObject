d2f83a48ce4aa1da130d25e8c6213fd3c4139529
Anas Ambri <anas.ambri@gmail.com>
2016-04-08 16:01:31 -0400

    Guard against passing  to PFObject

--- GRNPFObject/GRNPFObject.m
+++ GRNPFObject/GRNPFObject.m
@@ -14,4 +14,11 @@
     return [GRNPFQuery queryWithClassName:[(id<PFSubclassing>)self parseClassName]];
 }
 
+- (void)setObject:(id)object forKey:(NSString *)key {
+    if (object == nil) {
+        object = [NSNull null];
+    }
+    [self _setObject:object forKey:key onlyIfDifferent:NO];
+}
+
 @end
